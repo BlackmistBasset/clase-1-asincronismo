6e67097ffc608d240b5b7738432b072a571a315f
Basset <aldana.sorni@gmail.com>
2024-04-15 22:25:05 -0300

    trabajo de ejemplo completo

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const container = document.getElementById("main-container");
 const spinner = document.querySelector(".spinner_container");
+const nuevaAlumnaBtn = document.getElementById("nueva-alumna");
 
 const baseUrl = "https://661463082fc47b4cf27c3d2f.mockapi.io/api/alumnas";
 
@@ -93,7 +94,7 @@ const mostrarDetalleAlummna = (alumna) => {
         <img src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQgByBT5IiAT_a2x9pUVb4VMoOrlzHH7Jrzj-HB5jzHlR4lNLMS" alt="" class="card_img" />
         <p class="card_location">${location}</p>
         <button class="card_button_editar" data-cardId="${id}">Editar</button>
-        <button class="card_button_eliminar" data-cardId="${id}">Eliminar</button>
+        <button class="card_button_eliminar">Eliminar</button>
       </div>
 
        <form id="editar-alumna" class="hidden">
@@ -102,7 +103,16 @@ const mostrarDetalleAlummna = (alumna) => {
         <label for="">Location</label>
         <input type="text" name="" id="location-input" />
         <input type="submit" value="Editar" />
-      </form>`;
+      </form>
+       
+      <div class="modal_contenedor hidden">
+        <p>Estás seguro que deseas borrar la información de la alumna?</p>
+
+        <button id="confirmar-eliminar" data-cardId="${id}">Eliminar Alumna</button>
+        <button id="cancelar-eliminar">Cancelar</button>
+      </div>
+      
+      `;
 
     //funcion regresar
 
@@ -110,24 +120,137 @@ const mostrarDetalleAlummna = (alumna) => {
       .querySelector(".go_back")
       .addEventListener("click", () => getAlumnas(baseUrl));
 
+    const editarAlumnaForm = document.getElementById("editar-alumna");
+
+    //Mostrar Form Editar Alummna
     document
       .querySelector(".card_button_editar")
       .addEventListener("click", () => mostrarFormEditarAlumna(alumna));
 
-    const editarAlumnaForm = document.getElementById("editar-alumna");
-
-    //Mostrar Form Editar Alummna
+    const nameInput = document.getElementById("name-input");
+    const locationInput = document.getElementById("location-input");
 
     const mostrarFormEditarAlumna = (alumna) => {
-      const nameInput = document.getElementById("name-input");
-      const locationInput = document.getElementById("location-input");
       nameInput.value = alumna.name;
       locationInput.value = alumna.location;
       editarAlumnaForm.classList.remove("hidden");
     };
+
+    //Confirmar Editar una alumna
+
+    const confirmarEditarAlumna = (alumna) => {
+      const alumnaEdit = {
+        ...alumna,
+        location: locationInput.value,
+        name: nameInput.value,
+      };
+
+      fetch(`${baseUrl}/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(alumnaEdit),
+      })
+        .then((res) => {
+          if (res.ok) {
+            getDetalleAlumna(id);
+          }
+        })
+        .catch((err) => console.log(err));
+
+      // console.log(alumnaEdit);
+    };
+
+    editarAlumnaForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+      confirmarEditarAlumna(alumna);
+    });
+
+    //Mostrar modal eliminar alumna
+    const btnEliminarAlumna = document.querySelector(".card_button_eliminar");
+    const modalEliminarAlumna = document.querySelector(".modal_contenedor");
+
+    btnEliminarAlumna.addEventListener("click", () => {
+      modalEliminarAlumna.classList.remove("hidden");
+    });
+
+    //Confirmar eliminar alumna
+
+    document
+      .getElementById("confirmar-eliminar")
+      .addEventListener("click", (e) => {
+        fetch(`${baseUrl}/${e.currentTarget.dataset.cardid}`, {
+          method: "DELETE",
+        }).then((res) =>
+          res
+            .json()
+            .then((data) => {
+              getAlumnas(baseUrl);
+              // console.log(data);
+            })
+            .catch((err) => console.log(err))
+        );
+      });
+
+    //Cancelar eliminar alumna
+    document
+      .getElementById("cancelar-eliminar")
+      .addEventListener("click", () =>
+        modalEliminarAlumna.classList.add("hidden")
+      );
   }, 2000);
 };
 
-//Confirmar Editar una alumna
+//Crear una nueva alumna
+const formCrearNuevaAlumna = document.getElementById("nueva-alumna-form");
+const formContenedor = document.getElementById("form-container");
+//const cargarNuevaAlumnaBtn
+console.log(formCrearNuevaAlumna);
 
-const confirmarEditarAlumna = (alumnaId) => {};
+nuevaAlumnaBtn.addEventListener("click", () => {
+  container.innerHTML = "";
+  formContenedor.classList.remove("hidden");
+  const newNameInput = document.getElementById("new-name-input");
+  const newLocationInput = document.getElementById("new-location-input");
+  const newDescriptionTextarea = document.getElementById(
+    "new-description-textarea"
+  );
+
+  formCrearNuevaAlumna.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const nuevaAlumna = {
+      name: newNameInput.value,
+      description: newLocationInput.value,
+      location: newDescriptionTextarea.value,
+    };
+
+    fetch(`${baseUrl}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(nuevaAlumna),
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+      })
+      .then((data) => {
+        if (data) {
+          formCrearNuevaAlumna.reset();
+          formContenedor.classList.add("hidden");
+          getAlumnas(baseUrl);
+        } else {
+          alert(
+            "Lo sentimos, ocurrió un error. Vuelva a intentarlo más tarde."
+          );
+        }
+      })
+      .catch((err) => alert(err));
+
+    console.log(nuevaAlumna);
+  });
+});
